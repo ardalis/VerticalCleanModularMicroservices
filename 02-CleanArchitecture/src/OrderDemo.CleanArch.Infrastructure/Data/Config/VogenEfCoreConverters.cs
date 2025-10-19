98fffa5431b73f2b839924d8d8d59f2ac1ac20ac
Steve Smith <steve@kentsmiths.com>
2025-10-19 12:54:52 +0100

    Fix vogen missing conversion

--- 02-CleanArchitecture/src/OrderDemo.CleanArch.Infrastructure/Data/Config/VogenEfCoreConverters.cs
+++ 02-CleanArchitecture/src/OrderDemo.CleanArch.Infrastructure/Data/Config/VogenEfCoreConverters.cs
@@ -1,8 +1,10 @@
 ﻿using OrderDemo.CleanArch.Core.ContributorAggregate;
+using OrderDemo.CleanArch.Core.ProductAggregate;
 using Vogen;
 
 namespace OrderDemo.CleanArch.Infrastructure.Data.Config;
 
 [EfCoreConverter<ContributorId>]
 [EfCoreConverter<ContributorName>]
+[EfCoreConverter<ProductId>]
 internal partial class VogenEfCoreConverters;
